302393ad97feeaf48530cefa295b501c010b6ab9
Georges Younes <georges.r.younes@gmail.com>
2017-06-16 10:57:06 +0100

    remove dotted

--- include/dotted_type.hrl
+++
@@ -1,9 +0,0 @@
--define(DOTTED_AWSET_TYPE, dotted_awset).
--define(DOTTED_DWFLAG_TYPE, dotted_dwflag).
--define(DOTTED_EWFLAG_TYPE, dotted_ewflag).
--define(DOTTED_GCOUNTER_TYPE, dotted_gcounter).
--define(DOTTED_GSET_TYPE, dotted_gset).
--define(DOTTED_MVREGISTER_TYPE, dotted_mvregister).
--define(DOTTED_PNCOUNTER_TYPE, dotted_pncounter).
--define(DOTTED_RWSET_TYPE, dotted_rwset).
--define(DOTTED_TWOPSET_TYPE, dotted_twopset).
